17bf141fdcbd1def452d140e09d4b36659f16a7f
aditrio <30noadi@gmail.com>
2021-03-21 19:26:10 +0700

    DDA Done

--- script/main.gd
+++ script/main.gd
@@ -4,20 +4,15 @@ var x1 : int
 var x2 : int
 var y1 : int
 var y2 : int
-var deltaX : int
-var deltaY : int
-var step : int
-var x_inc : float
-var y_inc : float
-var x_next
-var y_next
+
+
+
 
 func _ready():
 	#$TileMap.set_cell(5,5,0)
 	#$debug/Label.text = str("Titik : \n" , $TileMap.get_used_cells_by_id(0)) 
 	$Option.add_item("DDA", 0)
 	$Option.add_item("Bresenhem", 1)
-	
 	
 
 func _on_Button_pressed():
@@ -42,26 +37,38 @@ func clear_text_input():
 	$coordB/y2.clear()
 
 func dda(x1:int, x2:int, y1:int, y2:int):
-	$TileMap.set_cell(x1,y1,0)
+	var deltaX : int
+	var deltaY : int
+	var step : float
+	var x_inc : float
+	var y_inc : float
+	var x_next : float
+	var y_next : float
+	
+	
 	deltaX = abs(x2 - x1)
 	deltaY = abs(y2 - y1)
+	
+	print(deltaX,",", deltaY)
+	
 	if deltaX > deltaY : 
 		step = deltaX
 	else : 
 		step = deltaY
-	x_inc = float(deltaX)/step
-	y_inc = float(deltaY)/step
-	x_next = int(round(float(x1)+x_inc))
-	y_next = int(round(float(y1)+y_inc))
 	
-	while(true) : 
-		$TileMap.set_cell(x_next, y_next, 0)
-		if x_next == x2 and y_next == y2 : 
-			break
-		x_next += int(round(x_inc))
-		y_next += int(round(y_inc))
+	x_inc = deltaX/step
+	y_inc = deltaY/step
+	x_next = float(x1)
+	y_next = float(y1)
+	print("x  : ", x_next, "| x2 : ", x2)
+	print("y  : ", y_next, "| y2 : ", y2)
+	while(int(round(x_next)) != x2 || int(round(y_next)) != y2):
+		$TileMap.set_cell(int(round(x_next)), int(round(y_next)), 0)
+		x_next+=x_inc
+		y_next+=y_inc
+		print("x : " , int(round(x_next)),"\ny : ", int(round(y_next)))
+	$TileMap.set_cell(int(round(x_next)), int(round(y_next)), 0)
 	
-
 func clear_cell():
 	var data = $TileMap.get_used_cells_by_id(0)
 	for item in data : 
